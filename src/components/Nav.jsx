4ac156e7801738e2dab8da3ccb64ee0b8fb14c2d
atapiamallea <a.tapia.mallea@gmail.com>
2026-05-14 23:16:09 +0200

    feat: header and footer styles added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router'
+import React from "react";
+import { Link, useLocation } from "react-router";
 import { useEffect, useState } from "react";
 
 const MOBILE_BREAKPOINT = 768;
@@ -11,24 +11,24 @@ function Nav() {
   const [isMobile, setIsMobile] = useState(
     window.innerWidth < MOBILE_BREAKPOINT,
   );
- 
+
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     };
- 
-    window.addEventListener('resize', handleResize);
- 
+
+    window.addEventListener("resize", handleResize);
+
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
-  }, []); 
- 
+  }, []);
+
   const baseClasses =
-'left-0 right-0 z-50 flex items-center bg-bg font-vietnam px-6 py-4 transition-colors duration-300';
-    const positionClasses = isMobile
-    ? 'fixed bottom-0 border-t border-gray-200 justify-around'
-    : 'top-0 border-b border-white/5 justify-start gap-12';
+    "left-0 right-0 z-50 flex items-center bg-bg font-vietnam px-6 py-4 transition-colors duration-300";
+  const positionClasses = isMobile
+    ? "fixed bottom-0 border-t border-gray-200 justify-around"
+    : "top-0 justify-start gap-12";
 
   const activeStyle = "text-primary relative transition-all";
   const inactiveStyle = "text-gris hover:text-text transition-all";
@@ -37,24 +37,36 @@ function Nav() {
     <>
       {/* NAVEGACIÓN DESKTOP (Se integra en el centro del Header) */}
       <nav className={`${baseClasses} ${positionClasses}`}>
-
-        <Link to="/" className={`${isActive('/') ? activeStyle : inactiveStyle} text-sm font-medium tracking-wide`}>       
+        <Link
+          to="/"
+          className={`${isActive("/") ? activeStyle : inactiveStyle} text-sm font-medium tracking-wide`}
+        >
           Movies
-          {isActive('/') && <div className="absolute -bottom-1 left-0 h-[2px] w-full bg-primary shadow-[0_0_8px_rgba(0,244,254,0.6)]" />}
+          {isActive("/") && (
+            <div className="absolute -bottom-1 left-0 h-[2px] w-full bg-primary shadow-[0_0_8px_rgba(0,244,254,0.6)]" />
+          )}
         </Link>
-        <Link to="/pricing" className={`${isActive('/pricing') ? activeStyle : inactiveStyle}`}>
+        <Link
+          to="/pricing"
+          className={`${isActive("/pricing") ? activeStyle : inactiveStyle} text-sm font-medium tracking-wide`}
+        >
           Pricing
-          {isActive('/pricing') && <div className="absolute -bottom-1 left-0 h-[2px] w-full bg-primary shadow-[0_0_8px_rgba(0,244,254,0.6)]" />}
+          {isActive("/pricing") && (
+            <div className="absolute -bottom-1 left-0 h-[2px] w-full bg-primary shadow-[0_0_8px_rgba(0,244,254,0.6)]" />
+          )}
         </Link>
-        <Link to="/contact" className={`${isActive('/contact') ? activeStyle : inactiveStyle}`}>
+        <Link
+          to="/contact"
+          className={`${isActive("/contact") ? activeStyle : inactiveStyle} text-sm font-medium tracking-wide`}
+        >
           Contact
-          {isActive('/contact') && <div className="absolute -bottom-1 left-0 h-[2px] w-full bg-primary shadow-[0_0_8px_rgba(0,244,254,0.6)]" />}
+          {isActive("/contact") && (
+            <div className="absolute -bottom-1 left-0 h-[2px] w-full bg-primary shadow-[0_0_8px_rgba(0,244,254,0.6)]" />
+          )}
         </Link>
       </nav>
-
-      
     </>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
